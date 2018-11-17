bddc403bc6be805d3824fd880ff6ccb918a148af
MDingas <a79089@alunos.uminho.pt>
2018-11-17 16:57:45 +0000

    funcionalidade de fazer shift do zoneddatetime

--- Project/CalcDateTimeApp/src/Controller/Class/CalcDateTimeZoneController.java
+++ Project/CalcDateTimeApp/src/Controller/Class/CalcDateTimeZoneController.java
@@ -6,9 +6,16 @@ import Utilities.Input;
 import Utilities.Menu;
 import View.Interface.InterfCalcDateTimeZoneView;
 
+import static Utilities.EnumDateTimeShiftMode.ADD;
+import static Utilities.EnumDateTimeShiftMode.SUB;
 import static Utilities.BusinessUtils.zoneDateTimeToString;
 import static Utilities.BusinessUtils.getAvailableTimeZoneIdsByPage;
 import static Utilities.BusinessUtils.clearConsole;
+
+import static java.time.temporal.ChronoUnit.*;
+
+import static java.lang.Math.abs;
+import static java.lang.System.out;
 
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -51,11 +58,71 @@ public class CalcDateTimeZoneController implements InterfCalcDateTimeZoneControl
             opcao = Input.lerString().toUpperCase();
             switch(opcao) {
                 case "C": flowConvertZone(); break;
+                case "D": flowShiftZoneDateTime(); break;
                 case "S": break;
-                default: System.out.println("Opcao Invalida!"); break;
+                default: out.println("Opcao Invalida!"); break;
             }
 
         } while(!opcao.equals("S"));
+    }
+
+    private void flowShiftZoneDateTime() {
+        String ld;
+        Menu menu = viewZoneTxt.getMenu(1);
+        String opcao;
+        do {
+            ld = buildZoneDateTimeTitle();
+            menu.addDateTimeToTitle(ld);
+            menu.show();
+            opcao = Input.lerString();
+            opcao = opcao.toUpperCase();
+            switch(opcao) {
+                case "DIA" : shiftDays(); break;
+                case "SEM" : shiftWeeks(); break;
+                case "MES" : shiftMonths(); break;
+                case "ANO" : shiftYears(); break;
+                case "S": break;
+                default: out.println("Opcão Inválida !"); break;
+            }
+        }
+        while(!opcao.equals("S"));
+    }
+
+
+    private void shiftDays() {
+        out.print("(+|-) número de dias: ");
+        int n = Input.lerInt();
+        if (n >= 0)
+            model.shiftDateTimeZone(abs(n), DAYS, ADD);
+        else
+            model.shiftDateTimeZone(abs(n), DAYS, SUB);
+    }
+
+    private void shiftWeeks() {
+        out.print("Número de semanas: ");
+        int n = Input.lerInt();
+        if (n >= 0)
+            model.shiftDateTimeZone(abs(n), WEEKS, ADD);
+        else
+            model.shiftDateTimeZone(abs(n), WEEKS, SUB);
+    }
+
+    private void shiftMonths() {
+        out.print("Número de meses: ");
+        int n = Input.lerInt();
+        if (n >= 0)
+            model.shiftDateTimeZone(abs(n), MONTHS, ADD);
+        else
+            model.shiftDateTimeZone(abs(n), MONTHS, SUB);
+    }
+
+    private void shiftYears() {
+        out.print("Número de anos: ");
+        int n = Input.lerInt();
+        if (n >= 0)
+            model.shiftDateTimeZone(abs(n), YEARS, ADD);
+        else
+            model.shiftDateTimeZone(abs(n), YEARS, SUB);
     }
 
 
@@ -65,7 +132,7 @@ public class CalcDateTimeZoneController implements InterfCalcDateTimeZoneControl
     // Pedir para que zona queremos mudar a data
     private void flowConvertZone() {
         flowShowAllAvailableTimezones();
-        System.out.print("Zona para qual converter(S para sair): ");
+        out.print("Zona para qual converter(S para sair): ");
         String answerZone = Input.lerString();
 
         if (!answerZone.equals(("S"))) {
@@ -85,15 +152,15 @@ public class CalcDateTimeZoneController implements InterfCalcDateTimeZoneControl
         String opcao;
         do {
             clearConsole();
-            allZoneidsByPage.get(pageIndex).forEach((String s) -> System.out.println("." + s));
-            System.out.println("Pagina (" + (pageIndex+1) + "/" + allZoneidsByPage.size() + ")");
-            System.out.println("(+) Proxima pagina (-) Previa pagina (S) Sair da listagem");
+            allZoneidsByPage.get(pageIndex).forEach((String s) -> out.println("." + s));
+            out.println("Pagina (" + (pageIndex+1) + "/" + allZoneidsByPage.size() + ")");
+            out.println("(+) Proxima pagina (-) Previa pagina (S) Sair da listagem");
             opcao = Input.lerString().toUpperCase();
             switch(opcao) {
                 case "+": if ((pageIndex + 1) < allZoneidsByPage.size()) { pageIndex++; } break;
                 case "-": if ((pageIndex - 1) >= 0) { pageIndex--; } break;
                 case "S": break;
-                default: System.out.println("Opcao Invalida!"); break;
+                default: out.println("Opcao Invalida!"); break;
             }
 
         } while(!opcao.equals("S"));
--- Project/CalcDateTimeApp/src/View/Class/CalcDateTimeZoneView.java
+++ Project/CalcDateTimeApp/src/View/Class/CalcDateTimeZoneView.java
@@ -22,11 +22,24 @@ public class CalcDateTimeZoneView implements InterfCalcDateTimeZoneView {
         //------------------------
         // CaldDateTimeZone
         //------------------------
-        op1 = new Opcao("Converter para fuso ....... ", "C");
-        op2 = new Opcao("Sair da aplicacao >>>>>>>>> ", "S");
-        List<Opcao> linhas = Arrays.asList(op1,op2);
+        op1 = new Opcao("Deslocar data ............. ", "D");
+        op2 = new Opcao("Converter para fuso ....... ", "C");
+        op3 = new Opcao("Sair da aplicacao >>>>>>>>> ", "S");
+        List<Opcao> linhas = Arrays.asList(op1,op2,op3);
         Menu menuCalcDateTimeZone = new Menu(linhas,"   Data: ");
         menusZoneTxt.addMenu(0,menuCalcDateTimeZone);
+
+        //------------------------
+        // Menu Shift ZoneDateTime
+        //------------------------
+        op1 = new Opcao("Deslocar dia ........... ", "Dia");
+        op2 = new Opcao("Deslocar semana ........ ", "Sem");
+        op3 = new Opcao("Deslocar mes ........... ", "Mes");
+        op4 = new Opcao("Deslocar ano ........... ", "Ano");
+        op5 = new Opcao("Voltar a Calc. Local >>> ", "S");
+        linhas = Arrays.asList(op1, op2, op3, op4, op5);
+        Menu menuShiftDateTime = new Menu(linhas, "   Data: ");
+        menusZoneTxt.addMenu(1, menuShiftDateTime);
 
         return menusZoneTxt;
     }
