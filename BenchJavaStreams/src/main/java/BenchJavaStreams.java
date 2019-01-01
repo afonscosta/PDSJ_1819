5df675aaed56cb0c81b9141a27211bd80bcffe37
afonscosta <afonso.costa.15@gmail.com>
2019-01-01 12:45:23 +0000

    Proposta de estrutura para generalização da solução. T2 Feita.

--- BenchJavaStreams/src/main/java/BenchJavaStreams.java
+++ BenchJavaStreams/src/main/java/BenchJavaStreams.java
@@ -3,17 +3,15 @@
  * @author fmm 2017
  */
 
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.AbstractMap.SimpleEntry;
-import java.util.ArrayList;
-import java.util.DoubleSummaryStatistics;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.util.*;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
@@ -22,6 +20,8 @@ import static java.util.stream.Collectors.toList;
 
 
 public class BenchJavaStreams {
+
+    private static final List<List<TransCaixa>> ltcs = new ArrayList<>();
 
     public static void memoryUsage() {
         final int mByte = 1024*1024;
@@ -72,30 +72,191 @@ public class BenchJavaStreams {
         return ltc;
     }
 
-    public static <R> SimpleEntry<Double,R> testeBoxGen(Supplier<? extends R> supplier) {
+    public static <R> SimpleEntry<Double,R> testeBoxGenW(Supplier<? extends R> supplier)  {
+        // com warmup de 5 runs
+        for (int i = 1; i <= 10; i++) supplier.get();
+        System.gc();
         Crono.start();
         R resultado = supplier.get();
         Double tempo = Crono.stop();
         return new SimpleEntry<Double,R>(tempo, resultado);
     }
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    private static void loadFiles(List<String> files) {
+        for (String f : files) {
+            Crono.start();
+            final List<TransCaixa> ltc= setup(f);
+            out.println("========================");
+            out.println(" Documento " + f.substring(20, f.length()-4));
+            out.println("========================");
+            out.println("Duração: " + Crono.stop()*1000 + " ms");
+            out.println("Transacções lidas: " + ltc.size());
+            out.println("\n");
+            ltcs.add(ltc);
+        }
+    }
 
-        String nomeFich = "TransCaixaResources/transCaixa1M.txt";
-        List<TransCaixa> ltc1;
+    private static Comparator<TransCaixa> transCaixaComparator = (TransCaixa tc1, TransCaixa tc2) -> {
+        if (Objects.equals(tc1, tc2)) return 0;
+        else if(tc1 == null) return -1;
+        else if(tc2 == null) return +1;
 
-        // LE O FICHEIRO DE TRANSACÇOES PARA List<TransCaixa> com Streams
-        Crono.start();
-        ltc1 = setup(nomeFich);
-        out.println("Setup com Streams: " + Crono.stop()*1000 + " ms");
-        out.println("Transacções lidas - Streams: " + ltc1.size());
+        if (tc1.getData().isBefore(tc2.getData())) {
+            return -1;
+        }
+        else if (tc1.getData().isAfter(tc2.getData())) {
+            return 1;
+        }
+        return tc1.getTrans().compareTo(tc2.getTrans());
+    };
 
-        final List<TransCaixa> ltc = new ArrayList<>(ltc1);
-
-        Supplier<String> debug = () -> {
-            ltc.stream().limit(10).forEach(tc -> out.println(tc));
-            return null;
+    private static Function<List<TransCaixa>, Supplier<SimpleEntry>> solucao2S = ltc -> {
+        Supplier<SimpleEntry> t2S = () -> {
+            int size = ltc.size();
+            List<TransCaixa> sortedList = ltc.stream()
+                                             .sorted(transCaixaComparator)
+                                             .collect(toList());
+            List<TransCaixa> l1 = sortedList.subList(0, size/3);
+            List<TransCaixa> l2 = sortedList.subList(size - size/3, size);
+            return new SimpleEntry<>(l1, l2);
         };
-        testeBoxGen(debug);
+        return t2S;
+    };
+
+    private static Function<List<TransCaixa>, Supplier<SimpleEntry>> solucao2PS = ltc -> {
+        Supplier<SimpleEntry> t2PS = () -> {
+            int size = ltc.size();
+            List<TransCaixa> sortedList = ltc.parallelStream()
+                                             .sorted(transCaixaComparator)
+                                             .collect(toList());
+            List<TransCaixa> l1 = sortedList.subList(0, size/3);
+            List<TransCaixa> l2 = sortedList.subList(size - size/3, size);
+            return new SimpleEntry<>(l1, l2);
+        };
+        return t2PS;
+    };
+
+    private static Function<List<TransCaixa>, Supplier<SimpleEntry>> solucao2L = ltc -> {
+        Supplier<SimpleEntry> t2L = () -> {
+            int size = ltc.size();
+            ltc.sort(transCaixaComparator);
+            List<TransCaixa> l1 = ltc.subList(0, size/3);
+            List<TransCaixa> l2 = ltc.subList(size - size/3, size);
+            return new SimpleEntry<>(l1, l2);
+        };
+        return t2L;
+    };
+
+    private static Function<List<TransCaixa>, Supplier<SimpleEntry>> solucao2TS = ltc -> {
+        Supplier<SimpleEntry> t2TS = () -> {
+            TreeSet<TransCaixa> ts = new TreeSet<>(transCaixaComparator);
+            ts.addAll(ltc);
+            List<TransCaixa> l1 = new ArrayList<>();
+            List<TransCaixa> l2 = new ArrayList<>();
+            int size = ts.size();
+            int i = 0;
+            for (TransCaixa tc : ts) {
+                if (i < size / 3) {
+                    l1.add(tc);
+                } else break;
+                i++;
+            }
+            i = 0;
+            for (TransCaixa tc : ts.descendingSet()) {
+                if (i < size/3) {
+                    l2.add(tc);
+                } else break;
+                i++;
+            }
+            return new SimpleEntry<>(l1, l2);
+        };
+        return t2TS;
+    };
+
+    private static Function<List<TransCaixa>, Function<List<SimpleEntry<String, Function>>, List<SimpleEntry<String, Supplier<SimpleEntry>>>>>
+    createSuppliers = ltc -> (lsols -> {
+        List<SimpleEntry<String, Supplier<SimpleEntry>>> lsups = new ArrayList<>();
+        for (SimpleEntry<String, Function> sol : lsols) {
+            lsups.add(new SimpleEntry<>(sol.getKey(), (Supplier<SimpleEntry>) sol.getValue().apply(ltc)));
+        }
+        return lsups;
+    });
+
+    private static List<List<List<SimpleEntry<String, SimpleEntry<Double, SimpleEntry>>>>> runTestes(List<List<SimpleEntry<String, Function>>> respostas) {
+        List<List<List<SimpleEntry<String, SimpleEntry<Double, SimpleEntry>>>>> resultados = new ArrayList<>();
+        for (List<SimpleEntry<String, Function>> respostasTi : respostas) {
+            List<List<SimpleEntry<String, SimpleEntry<Double, SimpleEntry>>>> resultadosTi = new ArrayList<>();
+            for (List<TransCaixa> ltc : ltcs) {
+                List<SimpleEntry<String, SimpleEntry<Double, SimpleEntry>>> resultadosResourceI = new ArrayList<>();
+                List<SimpleEntry<String, Supplier<SimpleEntry>>> suppsRespostasTi = createSuppliers.apply(ltc).apply(respostasTi);
+                for (SimpleEntry<String, Supplier<SimpleEntry>> supResposta : suppsRespostasTi) {
+                    SimpleEntry<Double, SimpleEntry> res = testeBoxGenW(supResposta.getValue());
+                    resultadosResourceI.add(new SimpleEntry<>(supResposta.getKey(), res));
+                }
+                resultadosTi.add(resultadosResourceI);
+            }
+            resultados.add(resultadosTi);
+        }
+        return resultados;
+    }
+
+    private static void genCSVTable(String filename, List<List<SimpleEntry<String, SimpleEntry<Double, SimpleEntry>>>> resultadosTi) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        FileWriter fileWriter = new FileWriter(filename + ".csv");
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        StringBuilder header = new StringBuilder();
+        header.append("# Transações");
+        header.append(";");
+        boolean headerDone = false;
+        int resource = 0;
+        for (List<SimpleEntry<String, SimpleEntry<Double, SimpleEntry>>> resultadosResourceI : resultadosTi) {
+            sb.append(ltcs.get(resource).size());
+            sb.append(";");
+            resource++;
+            for (SimpleEntry<String, SimpleEntry<Double, SimpleEntry>> resultado : resultadosResourceI) {
+                if (!headerDone) {
+                    header.append(resultado.getKey());
+                    header.append(";");
+                }
+                sb.append(resultado.getValue().getKey());
+                sb.append(";");
+            }
+            headerDone = true;
+            header.delete(header.length()-1, header.length());
+            header.append("\n");
+            sb.delete(sb.length()-1, sb.length());
+            sb.append("\n");
+        }
+        printWriter.print(header.toString());
+        printWriter.print(sb.toString().replaceAll("\\.", ","));
+        printWriter.close();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        List<String> file = Arrays.asList("TransCaixaResources/transCaixa1M.txt");
+
+        // Lista dos ficheiros de dados
+        List<String> files = Arrays.asList("TransCaixaResources/transCaixa1M.txt",
+                                           "TransCaixaResources/transCaixa2M.txt",
+                                           "TransCaixaResources/transCaixa4M.txt",
+                                           "TransCaixaResources/transCaixa6M.txt");
+        // Load dos dados
+        loadFiles(files);
+
+        List<List<SimpleEntry<String, Function>>> testes = new ArrayList<>();
+        List<SimpleEntry<String, Function>> respostasT2 = Arrays.asList(new SimpleEntry<>("List", solucao2L),
+                                                                        new SimpleEntry<>("TreeSet", solucao2TS),
+                                                                        new SimpleEntry<>("Streams", solucao2S),
+                                                                        new SimpleEntry<>("Parallel Streams", solucao2PS));
+        testes.add(respostasT2);
+
+        List<List<List<SimpleEntry<String, SimpleEntry<Double, SimpleEntry>>>>> resultados = runTestes(testes);
+
+        int testeNum = 1;
+        for (List<List<SimpleEntry<String, SimpleEntry<Double, SimpleEntry>>>> resultadosTi : resultados) {
+            genCSVTable("t" + testeNum, resultadosTi);
+        }
     }
 }
